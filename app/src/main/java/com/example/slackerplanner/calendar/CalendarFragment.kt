1e0a7a17005e8b5e6d133b2669cc1a76e74f4a4f
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-01-03 23:29:46 +0900

    modify chart ui

--- app/src/main/java/com/example/slackerplanner/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/slackerplanner/calendar/CalendarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
